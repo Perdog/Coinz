65440069a065c00cee54759375c0c1f41ad55d9b
Jacob Perry <jacob_perry@hotmail.ca>
2012-02-13 11:35:13 -0500

    Fixing the events
    Forgot the handler list, and bukkit likes to spaz when you forget them :P

--- src/dev/mCraft/Coinz/api/Vault/VaultStoreEvent.java
+++ src/dev/mCraft/Coinz/api/Vault/VaultStoreEvent.java
@@ -1,12 +1,15 @@
 package dev.mCraft.Coinz.api.Vault;
 
 import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
 import org.getspout.spoutapi.inventory.SpoutItemStack;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class VaultStoreEvent extends Event {
 
 	private static final long serialVersionUID = -4984092047714434077L;
+
+	private static final HandlerList handlers = new HandlerList();
 	
 	private SpoutPlayer player;
 	private SpoutItemStack item;
@@ -35,6 +38,22 @@ public class VaultStoreEvent extends Event {
 	 */
 	public SpoutPlayer getPlayer() {
 		return player;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @return Handler List
+	 */
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @return Handler List
+	 */
+	public static HandlerList getHandlerList() {
+		return handlers;
 	}
 
 }
--- src/dev/mCraft/Coinz/api/Vault/VaultTakeEvent.java
+++ src/dev/mCraft/Coinz/api/Vault/VaultTakeEvent.java
@@ -1,12 +1,15 @@
 package dev.mCraft.Coinz.api.Vault;
 
 import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
 import org.getspout.spoutapi.inventory.SpoutItemStack;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
 public class VaultTakeEvent extends Event {
 
 	private static final long serialVersionUID = 3871139411791321784L;
+	
+	private static HandlerList handlers = new HandlerList();
 	
 	private SpoutPlayer player;
 	private SpoutItemStack item;
@@ -36,6 +39,22 @@ public class VaultTakeEvent extends Event {
 	 */
 	public SpoutPlayer getPlayer() {
 		return player;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @return Handler List
+	 */
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+	
+	/**
+	 * {@inheritDoc}
+	 * @return Handler List
+	 */
+	public static HandlerList getHandlerList() {
+		return handlers;
 	}
 
 }
